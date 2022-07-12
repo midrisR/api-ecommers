3b93e8136fbad0c409fd28ba8ae94d055eb1a9c8
midrisR <midrisramdhani96@gmail.com>
2022-07-13 00:02:21 +0700

    edit quantity updated

--- controller/chartController.js
+++ controller/chartController.js
@@ -1,36 +1,37 @@
-const Chart = require("../model/charts");
-const mongoose = require("mongoose");
+const Chart = require('../model/charts');
+const mongoose = require('mongoose');
+
 const index = async (req, res) => {
-  try {
-    const chart = await Chart.find().populate("product");
-    return res.status(200).json(chart);
-  } catch (error) {
-    console.log(error);
-  }
+	try {
+		const chart = await Chart.find().populate('product');
+		return res.status(200).json(chart);
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 const create = async (req, res) => {
-  const filter = {
-    product: mongoose.Types.ObjectId("62cd398a0b690a3c27eae3d4"),
-  };
-  const find = await Chart.find(filter);
-
-  try {
-    if (find) {
-      const chart = await Chart.findOneAndUpdate(
-        filter,
-        {
-          quantity: req.body.quantity,
-        },
-        { new: true }
-      );
-      return res.status(200).json(chart);
-    }
-    const chart = await Chart.create(req.body);
-    return res.status(200).json(chart);
-  } catch (error) {
-    console.log(error);
-  }
+	const { quantity } = req.body;
+	const filter = {
+		product: mongoose.Types.ObjectId(req.body.product),
+	};
+	const find = await Chart.findOne(filter);
+	console.log(find);
+	try {
+		if (find !== null) {
+			const update = await Chart.findOneAndUpdate(
+				filter,
+				{ quantity: quantity + find.quantity },
+				{ new: true }
+			);
+			return res.status(200).json(update);
+		} else {
+			const chart = await Chart.create(req.body);
+			return res.status(200).json(chart);
+		}
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 module.exports = { index, create };
--- controller/productsController.js
+++ controller/productsController.js
@@ -1,21 +1,13 @@
-const Products = require("../model/products");
-const { faker } = require("@faker-js/faker");
+const Products = require('../model/products');
+const { faker } = require('@faker-js/faker');
 
 const index = async (req, res) => {
-  const arr = [];
-  for (let i = 0; i < 10; i++) {
-    arr.push({
-      name: faker.commerce.productName(),
-      price: faker.commerce.price(100, 200, 0),
-      image: faker.image.animals(500, 500),
-    });
-  }
-  try {
-    const create = await Products.create(arr);
-    return res.json(create);
-  } catch (error) {
-    console.log(error);
-  }
+	try {
+		const create = await Products.find();
+		return res.json(create);
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 module.exports = { index };
